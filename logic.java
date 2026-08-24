88854c9bbdf64ae0892e2dc447b43c9d80c447cc
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-24 17:09:45 +0530

    made calculator

--- logic.java
+++ logic.java
@@ -28,19 +28,88 @@ public class logic {
 
     // Check whether a number is divisible by both 5 and 11.
 
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     int a = sc.nextInt();
+
+    //     if(a % 5 == 0 && a % 11 == 0) {
+    //         System.out.println("a is divisible by both 5 and 11");
+    //     }  else {
+    //         System.out.println("n is not divisible by both 5 and 11");
+    //     }
+
+    //     sc.close();
+
+
+    // }
+
+//     Take marks and print grade:
+// 90–100 → A
+// 80–89 → B
+// 70–79 → C
+// 60–69 → D
+// below 60 → F
+
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("marks: ");
+    //     int marks = sc.nextInt();
+
+    //     if(marks <= 100 && marks >=90) {
+    //         System.out.println("grade A");
+    //     } else if (marks <= 89 && marks >= 80)  {
+    //         System.out.println("grade B");
+    //     } else if (marks <=79 && marks >= 70) {
+    //         System.out.println("grade C");
+    //     } else if (marks <= 69 && marks >= 60) {
+    //         System.out.println("garde D");
+    //     } else {
+    //         System.out.println("failed");
+
+    //     }
+
+
+    //     sc.close();
+
+    //     }
+
+    // Create a simple calculator using +, -, *, /, %.
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("num1: ");
+        double num1 = sc.nextDouble();
 
-        int a = sc.nextInt();
+        System.out.println("num2: ");
+        double num2 = sc.nextDouble();
 
-        if(a % 5 == 0 && a % 11 == 0) {
-            System.out.println("a is divisible by both 5 and 11");
-        }  else {
-            System.out.println("n is not divisible by both 5 and 11");
+        System.out.print("choose operator +, -, *, /, % : ");
+        String operator = sc.next();
+
+        if (operator.equals("+")) {
+            System.out.println("result =" + (num1 + num2));
+        } else if (operator.equals("-")) {
+            System.out.println("result =" + (num1 - num2));
+        } else if (operator.equals("*")) {
+            System.out.println("result =" + (num1*num2));
+        } else if (operator.equals("/")) {
+            System.out.println("result =" + (num1/num2));
+        } else if (operator.equals("%")) {
+            System.out.println("result =" + (num1%num2));
+        } else{
+            System.out.println("invalid operator");
         }
 
         sc.close();
-
-
+        }
     }
-}
+    
+
+
+
+
+
+
+
