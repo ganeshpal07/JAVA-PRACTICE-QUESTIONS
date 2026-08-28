4d0122071406e8e95bc8ec914699f792baaa175d
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-29 00:07:09 +0530

    practiced loops

--- Loops.java
+++ Loops.java
@@ -1,8 +1,51 @@
 // Print numbers from 100 to 1.
 
 public class Loops {
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
+    //     for( int i = 10; i>=1; i--) {
+    //         System.out.println(i);
+    //     }
+
         
+    // }
+
+    // Print all even numbers from 1 to 100.
+
+    // public static void main(String[] args) {
+    //     for(int i = 1; i<=100; i++) {
+    //         if(i%2==0) {
+    //             System.out.println(i);
+    //         } 
+    //     }
+
+    // }
+
+    // Print the sum of numbers from 1 to n.
+
+    // public static void main(String[] args) {
+    //     int n = 15;
+    //     int sum = 0;
+    //     for(int i = 1; i<=n; i++) {
+            // sum = sum + i;
+                  // Purane sum mein current i ko add karke, result ko wapas sum mein store karo.
+    //     }
+    //     System.out.println(sum);
+    // }
+
+    // Print the sum of all even numbers from 1 to n.
+
+    public static void main(String[] args) {
+
+    int n = 10;
+    int sum = 0;
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 0) {
+            sum = sum + i;
+        }
     }
+
+    System.out.println(sum);
+}
     
 }
--- logic.java
+++ logic.java
@@ -110,20 +110,49 @@ public class logic {
 
     // Check whether a year is a leap year.
 
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //      System.out.println("enter year: ");
+    //      int year = sc.nextInt();
+
+    //     if (year %  4 == 0 || year % 400 == 0) {
+    //         System.out.println("this is a leap year");
+    //     } else {
+    //         System.out.println("not a leap year");
+    //     }
+    //     sc.close();
+
+    // }
+
+
+    // Given a number from 1–7, print the corresponding day of the week.
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         System.out.println("enter year: ");
-         int year = sc.nextInt();
+        int num = sc.nextInt();
 
-        if (year %  4 == 0 || year % 400 == 0) {
-            System.out.println("this is a leap year");
-        } else {
-            System.out.println("not a leap year");
+        switch(num) {
+            case 1 : System.out.println("Monday");
+            break;
+            case 2 : System.out.println("Tuesday");
+            break;
+            case 3 : System.out.println("Wednesday");
+            break;
+            case 4 : System.out.println("Thursday");
+            break;
+            case 5 : System.out.println("Friday");
+            break;
+            case 6 : System.out.println("saturday");
+            break;
+            case 7 : System.out.println("Sunday");
+            // break;
+            default : System.out.println("ivalid");
+            sc.close();
         }
-        sc.close();
 
     }
-    }
+}
     
 
 
