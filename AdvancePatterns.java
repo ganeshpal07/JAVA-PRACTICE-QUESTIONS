acad703b20399d3ec04c83cd723555555451ea28
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-30 23:34:46 +0530

    advance patterns questions

---
+++ AdvancePatterns.java
@@ -0,0 +1,59 @@
+/*
+1
+121
+12321
+1234321
+123454321 print the pattern or take user input.
+*/
+
+import java.util.*;
+
+public class AdvancePatterns{
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+
+    //     for(int i=1; i<=n; i++) {
+    //         // increasing part
+    //         for(int j=1; j<=i; j++) {
+    //             System.out.print(j);
+    //         }
+    //         // decreasing part
+    //         for(int k=i-1; k>=1; k--) {
+    //             System.out.print(k);
+    //         }
+    //         System.out.println();
+    //         sc.close();
+
+    //     }
+    // }
+
+
+    /*
+    5
+    545
+    54345
+    5432345
+    543212345 perform */
+
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n; i>=1; i--) {
+            // decreasing part
+            for(int j=5; j>=i; j--) {
+                System.out.print(j);
+            }
+            // increasing part
+            for(int k=i+1; k<=5; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+            sc.close();
+
+        }
+    }
+
+    
+}
