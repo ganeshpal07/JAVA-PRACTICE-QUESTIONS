45d9485314ff083b3371eaabe54c379fc8070274
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-31 13:11:42 +0530

    practice questions

--- AdvancePatterns.java
+++ AdvancePatterns.java
@@ -36,22 +36,49 @@ public class AdvancePatterns{
     5432345
     543212345 perform */
 
-     public static void main(String[] args) {
+    //  public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+
+    //     for(int i=n; i>=1; i--) {
+    //         // decreasing part
+    //         for(int j=5; j>=i; j--) {
+    //             System.out.print(j);
+    //         }
+    //         // increasing part
+    //         for(int k=i+1; k<=5; k++) {
+    //             System.out.print(k);
+    //         }
+    //         System.out.println();
+    //         sc.close();
+
+    //     }
+    // }
+
+
+
+    /*
+    1
+    212
+    32123
+    4321234
+    543212345 */
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i=n; i>=1; i--) {
-            // decreasing part
-            for(int j=5; j>=i; j--) {
+        for(int i=1; i<=n; i++) {
+            // decreasing
+            for(int j=i; j>=1; j--) {
                 System.out.print(j);
             }
-            // increasing part
-            for(int k=i+1; k<=5; k++) {
+            // increasing
+            for(int k=2; k<=i; k++) {
                 System.out.print(k);
             }
             System.out.println();
             sc.close();
-
         }
     }
 
