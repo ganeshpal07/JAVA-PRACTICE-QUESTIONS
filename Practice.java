aee719a57ccaefb8e3dc7a29e12828d55ecf4c06
ganeshpal07 <ganeshpal1516@gmail.com>
2026-09-04 12:51:12 +0530

    practice questions

--- Practice.java
+++ Practice.java
@@ -25,22 +25,66 @@ Input 12345 → Output 54321 */
 // print first digit of a number:
 
 
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int number = sc.nextInt();
+//     int digit = 0;
+//     // digit ek variable initialize kia h jiski staring value 0 h.
+
+//     while(number>0) {
+//         // jab tak number 0 se bada hoga tab tak loop chalegaa.
+//         digit = number;
+//         // number ki current value ki copy digit m store ho jaaegi.
+//         number = number/10;
+//         /* iss loop m number jab tak 0 se bada hoga 
+//         tab tak vo 10 se divide hota rahegga; jaise hi condition false
+//         hogi loop band ho jaaega */
+//     }
+//     System.out.println("FirstDigit is :" + digit);
+//     sc.close();
+// }
+
+
+// print last digit of a number;
+
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int number = sc.nextInt();
+//     int lastdigit = number%10;
+
+//     if(number>0) {
+//         System.out.println("Last Digit is :" +lastdigit);
+//     } else {
+//         System.out.println("invalid");
+//     }
+//     sc.close();
+// }
+
+
+// print the sum of first and last digit
+
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int number = sc.nextInt();
-    int digit = 0;
-    // digit ek variable initialize kia h jiski staring value 0 h.
+    
+    if(number>0) {
+        int firstdigit = 0;
+        int lastdigit = number%10;
+    
+        while(number>0) {
+            firstdigit = number;
+            number = number/10;
+            
+        }
+        int sum = lastdigit + firstdigit;
 
-    while(number>0) {
-        // jab tak number 0 se bada hoga tab tak loop chalegaa.
-        digit = number;
-        // number ki current value ki copy digit m store ho jaaegi.
-        number = number/10;
-        /* iss loop m number jab tak 0 se bada hoga 
-        tab tak vo 10 se divide hota rahegga; jaise hi condition false
-        hogi loop band ho jaaega */
+        System.out.println("FirstDigit =" +firstdigit);
+        System.out.println("LastDigit =" +lastdigit);
+        System.out.println("sum is:" +sum);
+
+    } else {
+        System.out.println("invalid");
     }
-    System.out.println("FirstDigit is :" + digit);
     sc.close();
 }
     
